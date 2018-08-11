0a783486339a8383198725224d0f51b172931c73
Valentino <vskobljanec@gmail.com>
2018-08-11 20:23:09 +0200

    feat: Association class in Console project

--- projects/SampleConsoleApplication/Program.cs
+++ projects/SampleConsoleApplication/Program.cs
@@ -3,21 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static SampleConsoleApplication.Contact;
 
 namespace SampleConsoleApplication
 {
+
     class Program
     {
         static void Main(string[] args)
         {
             Email hotmail = new Email("perica@hotmail.com");
-            Contact contact = new Contact("ivica",hotmail);
+            Contact contact = new Contact("ivica", hotmail);
+            string read = Console.ReadLine();
+            Email mejl = new Email(read);
+            contact.AddEmail(mejl);
             Console.WriteLine("\r\nProcess done. Press any key to exit.");
             Console.ReadKey();
             return;
         }
     }
-
     internal class Contact
     {
         private readonly string _name = string.Empty;
@@ -36,8 +40,6 @@ namespace SampleConsoleApplication
         public string Gender { get; set; }
 
         public string Residence { get; set; }
-
-        //private readonly List<Email> _emails = null;
 
         public long PhoneNumber { get; set; }
 
@@ -61,24 +63,30 @@ namespace SampleConsoleApplication
         public void AddEmail(Email email)
         {
             _emails.Add(email);
-            Console.WriteLine("Email called '{0}' added to contact called '{1}'", email.Name,_name);
+            Console.WriteLine("Email called '{0}' added to contact called '{1}'", email.Name, _name);
+            int s = _emails.Count;
+            for (int i = 0; i < s; ++i)
+            {
+                Console.WriteLine("'{0}'. mail is '{1}'\n",i+1,_emails[i].Name);
+            }
         }
-    }
-    internal class Email
-    {
-        private string _emailName = string.Empty;
-    
-        public Email (string name)
+        internal class Email
         {
-            _emailName = name;
-            Console.WriteLine("New email created called '{0}'", name);
+            private string _emailName = string.Empty;
+
+            public Email(string name)
+            {
+                _emailName = name;
+                Console.WriteLine("New email created called '{0}'", name);
+            }
+
+            public string Name
+            {
+                get { return _emailName; }
+
+                set { _emailName = value; }
+            }
         }
 
-        public string Name
-        {
-            get { return _emailName; }
-
-            set { _emailName = value;  }
-        }
     }
 }
