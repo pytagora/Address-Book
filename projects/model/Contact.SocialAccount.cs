fd1c0d6f709b1b05f3ef29764550366b32437d7b
Valentino <vskobljanec@gmail.com>
2018-08-15 18:09:50 +0200

    fix: Remove IEnumerable<T> from Contact class

--- projects/model/Contact.SocialAccount.cs
+++ projects/model/Contact.SocialAccount.cs
@@ -7,71 +7,16 @@ namespace Model
     {
         public class SocialAccount
         {
-            public string Id { get; set; }
-            public SocialAccount(string id)
-            {
-                Id = id;
-            }
-        }
-        public class SocialAccounts : IEnumerable
-        {
-            private SocialAccount[] socialAccounts;
-            public SocialAccounts(SocialAccount[] socialAccountArray)
-            {
-                int socialAccountArrayLength = socialAccountArray.Length;
-                socialAccounts = new SocialAccount[socialAccountArrayLength];
-                for (int i = 0; i < socialAccountArrayLength; ++i)
-                {
-                    socialAccounts[i] = socialAccountArray[i];
-                }
-            }
-            IEnumerator IEnumerable.GetEnumerator()
-            {
-                return (IEnumerator) GetEnumerator();
-            }
-            public SocialAccountsEnum GetEnumerator()
-            {
-                return new SocialAccountsEnum(socialAccounts);
-            }
-        }
-        public class SocialAccountsEnum : IEnumerator
-        {
-            public SocialAccount[] socialAccounts;
-            int position = -1;
-            public SocialAccountsEnum(SocialAccount[] list)
-            {
-                socialAccounts = list;
-            }
-            public bool MoveNext()
-            {
-                ++position;
-                return (position < socialAccounts.Length);
-            }
-            public void Reset()
-            {
-                position = -1;
-            }
-            object IEnumerator.Current
+            private string ID { get; set; }
+            private string SocialNetwork { get; set; }
+            /*public string ID
             {
                 get
                 {
-                    return Current;
+
                 }
-            }
-            public SocialAccount Current
-            {
-                get
-                {
-                    try
-                    {
-                        return socialAccounts[position];
-                    }
-                    catch (IndexOutOfRangeException)
-                    {
-                        throw new InvalidOperationException();
-                    }
-                }
-            }
+            }*/
+            public 
         }
     }
 }
